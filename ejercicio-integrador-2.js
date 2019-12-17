ff1828b78ab2e34ba17b847124d6a99144ac25bb
fpezcara <fpezcara@gmail.com>
2019-12-17 23:42:10 +0000

    Agregar arreglado

--- ejercicio-integrador-2.js
+++ ejercicio-integrador-2.js
@@ -6,8 +6,9 @@ const usuarios = [
 ];
 
 let accion = "";
+let realizarNuevamente = ""; 
 
-while (accion == !"SALIR") {
+while (accion !== "SALIR") {
   let accion = prompt(`¡Bienvenido!
 Por favor, elija una de las siguientes operaciones:
 - AGREGAR un usuario
@@ -16,7 +17,7 @@ Por favor, elija una de las siguientes operaciones:
 - MODIFICAR un usuario
 - ELIMINAR un usuario
 - SALIR del programa`);
-
+do {
   if (accion.toUpperCase() === "AGREGAR") {
     let nombreIngresado = prompt("Por favor ingrese el nombre");
     let telefonoIngresado = prompt("Por favor ingrese el número de teléfono");
@@ -35,43 +36,13 @@ Por favor, elija una de las siguientes operaciones:
     if (confirmacionUsuarioAAgregar.toUpperCase() === "NO") {
       alert("La operación se ha cancelado");
     }
-
-    let realizarNuevamente = prompt("¿Desea realizar esta acción nuevamente?");
-    if (realizarNuevamente.toUpperCase() === "SI") {
-      for (let i = 0; i < usuarios.length; i++) {
-        let nombreIngresado = prompt("Por favor ingrese el nombre");
-        let telefonoIngresado = prompt(
-          "Por favor ingrese el número de teléfono"
-        );
-        let emailIngresado = prompt("Por favor ingrese el e-mail");
-        let usuarioAAgregar = `
-      - ${nombreIngresado} 
-      - ${telefonoIngresado} 
-      - ${emailIngresado}`;
-        let confirmacionUsuarioAAgregar = prompt(`Nuevo usuario ingresado: 
-    ${usuarioAAgregar} 
-      ¿Desea confirmar la operación?`);
-        if (confirmacionUsuarioAAgregar.toUpperCase() === "SI") {
-          usuarios.push(usuarioAAgregar);
-          alert("La operación ha sido exitosa");
-        }
-        if (confirmacionUsuarioAAgregar.toUpperCase() === "NO") {
-          alert("La operación se ha cancelado");
-        }
-        if (realizarNuevamente.toUpperCase() === "NO") {
-          accion = prompt(`¡Bienvenido!
-  Por favor, elija una de las siguientes operaciones:
-  - AGREGAR un usuario
-  - OBTENER un usuario
-  - LISTAR todos los usuarios
-  - MODIFICAR un usuario
-  - ELIMINAR un usuario
-  - SALIR del programa`);
-        }
-      }
-    }
+    realizarNuevamente = prompt("¿Desea realizar esta operación nuevamente?")
   }
+}
+while (realizarNuevamente.toUpperCase() !== "NO")
+
   if (accion.toUpperCase() === "SALIR") {
     alert("¡Gracias por visitarnos!");
-  }
+  
+}
 }
