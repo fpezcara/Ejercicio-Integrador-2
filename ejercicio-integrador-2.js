7e4bcbae41e02a9b27a66d3d533be6dec27d889f
fpezcara <fpezcara@gmail.com>
2019-12-18 01:23:20 +0000

    agregué ID y converti string en array

--- ejercicio-integrador-2.js
+++ ejercicio-integrador-2.js
@@ -1,15 +1,15 @@
 const usuarios = [
-  ["Carla", "1545628984", "carla@gmail.com"],
-  ["Pedro", "1545251245", "pedro@gmail.com"],
-  ["Lucas", "1523357849", "lucas@gmail.com"],
-  ["Ana", "15789456", "ana@gmail.com"]
+  ["0", "Carla", "1545628984", "carla@gmail.com"],
+  ["1", "Pedro", "1545251245", "pedro@gmail.com"],
+  ["2", "Lucas", "1523357849", "lucas@gmail.com"],
+  ["3", "Ana", "15789456", "ana@gmail.com"]
 ];
 
 let accion = "";
 let realizarNuevamente = ""; 
 
 while (accion !== "SALIR") {
-  let accion = prompt(`¡Bienvenido!
+   accion = prompt(`¡Bienvenido!
 Por favor, elija una de las siguientes operaciones:
 - AGREGAR un usuario
 - OBTENER un usuario
@@ -22,15 +22,15 @@ do {
     let nombreIngresado = prompt("Por favor ingrese el nombre");
     let telefonoIngresado = prompt("Por favor ingrese el número de teléfono");
     let emailIngresado = prompt("Por favor ingrese el e-mail");
-    let usuarioAAgregar = `
+    let usuarioAAgregar = [`${usuarios.length}`, nombreIngresado, telefonoIngresado, emailIngresado];
+    let confirmacionUsuarioAAgregar = prompt(`Nuevo usuario ingresado: 
       - ${nombreIngresado} 
       - ${telefonoIngresado} 
-      - ${emailIngresado}`;
-    let confirmacionUsuarioAAgregar = prompt(`Nuevo usuario ingresado: 
-    ${usuarioAAgregar} 
+      - ${emailIngresado} 
       ¿Desea confirmar la operación?`);
     if (confirmacionUsuarioAAgregar.toUpperCase() === "SI") {
       usuarios.push(usuarioAAgregar);
+      console.log(usuarios)
       alert("La operación ha sido exitosa");
     }
     if (confirmacionUsuarioAAgregar.toUpperCase() === "NO") {
@@ -43,6 +43,7 @@ while (realizarNuevamente.toUpperCase() !== "NO")
 
   if (accion.toUpperCase() === "SALIR") {
     alert("¡Gracias por visitarnos!");
+    break;
   
 }
 }
